6a5279648a52cda75ab0a11c78648f0d6f95f339
ferencyim <ferencyim@gmail.com>
2023-05-05 20:51:35 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/uniflykbase/",
 
   lang: "zh-CN",
   title: "飞行笔记",
